99c7897eeb0c2a9e55ea6f6c99b303e5187c1d30
azrishrf <ishrafazri@gmail.com>
2023-07-13 21:51:45 +0800

    update

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,6 @@
 import { ref } from "vue";
 
-export const baseAPI = "https://cyber-tuition-backend.onrender.com";
+export const baseAPI = "https://cybertuition-backend.onrender.com";
 // export const baseAPI = "http://localhost:3001";
 
 export const pelajar = ref([]);
